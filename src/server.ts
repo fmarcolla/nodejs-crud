b22da5ddc1edda0914885a32aeacd9cbf535ac2a
Marcolla <felipem.marcolla@gmail.com>
2023-04-08 18:59:58 -0300

    Separado logica do app e de server

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
 import "reflect-metadata"; 
-import express from "express";
+import { app } from "./app";
 import { AppDataSource } from './database/data-source';
-import { routes } from "./routes"
 
-const app = express();
-app.use(express.json());
-app.use(routes);
 
 AppDataSource.initialize()
     .then(() => {
         app.listen(3000, () => console.log("Server is running")); 
     })
     .catch((error) => console.log(error))
+
